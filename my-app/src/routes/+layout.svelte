4280547387c2bf70c34887709799e206d28fdf8b
cooper-bocko-uga <105941591+cooperbocko@users.noreply.github.com>
2023-07-13 21:03:27 -0400

    formatted the file structure/home page
    was able to get my ideas out and get the file structure sorted for the website. I was also able to get the about/home page to look about the way I want with some minimal updates being needed. not really using anything i read from the svelte docs but hopefully I can do a project using it and have a better understanding of it.

--- my-app/src/routes/+layout.svelte
+++ my-app/src/routes/+layout.svelte
@@ -1,7 +1,26 @@
+<style>
+	nav {
+		font-family: 'Courier New', Courier, monospace;
+
+		margin-right: 10px;
+
+		display: flex;
+		justify-content: flex-end;
+		gap: 15px;
+	}
+
+	a, a:visited {
+		text-decoration: none;
+		color: red;
+	}
+
+</style>
 
 <nav>
 	<a href="/">home</a>
 	<a href="/about">about</a>
+	<a href="/projects">projects</a>
+	<a href="/contact">contact</a>
 </nav>
 
 <slot />
--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>home</h1>
-<p>this is the home page.</p>
+<style>
+    div {
+        font-family: 'Courier New', Courier, monospace;
+
+        display: flex;
+        flex-direction: column;
+        gap: 0px;
+
+        margin-top: 100px;
+        margin-left: 250px;
+    }
+
+    h1 {
+        margin-bottom: 0;
+    }
+
+    a, a:visited {
+		text-decoration: none;
+		color: red;
+	}
+</style>
+
+
+<div>
+
+<h1>hey, i  am cooper.</h1>
+<h1>i am a software engineer.</h1>
+<p>i am currently pursuing a degree in computer science at the university of georgia.</p>
+<p>i am an avid runnner and am apart of the track and field team at uga.</p>
+<p>here is my <a href="https://www.linkedin.com/in/cooperbocko">linkedin</a> and <a href="https://github.com/cooperbocko">github</a> accounts</p>
+
+</div>
+
+
+
